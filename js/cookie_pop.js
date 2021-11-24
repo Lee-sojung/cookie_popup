3bf3cab8d57cc49305e54484f046a82a68dfb07a
Lee-sojung <thwjd3698@gmail.com>
2021-11-24 12:07:31 +0900

    step3 es6 class로 변경

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -4,57 +4,64 @@
    document.cookie = `${name}=${val}; path=/; expires = ${due}`;
    }
 */
+class cookiePop{
 
-const btnDel = document.querySelector(".del");
-const btnView = document.querySelector(".view");
-const popup = document.querySelector("#popup");
-const btnClose = popup.querySelector(".close");
+   constructor(){
+      this.btnDel = document.querySelector(".del");
+      this.btnView = document.querySelector(".view");
+      this.popup = document.querySelector("#popup");
+      this.btnClose = this.popup.querySelector(".close");
+   
+      this.isCookie = document.cookie.indexOf("today=done");
+      this.isOn;
+   
+   
+   
+      /*
+      (isCookie == -1) ? isOn = "block" : isOn ="none"; 아래와 같은 코드
+      */
+      if(this.isCookie == -1){ // -1 => 쿠키가 없다면
+      console.log("쿠키없음");
+      this.isOn = "block";
+      }else{
+      console.log("쿠키있음");
+      this.isOn = "none";
+      }
+      this.popup.style.display = this.isOn;
+   
+   
+   
+      //쿠키확인 이벤트
+      this.btnView.addEventListener("click", e=>{
+      e.preventDefault();
+      console.log(document.cookie);
+   
+      });
+   
+      //쿠키 확인 버튼 이벤트
+      this.btnDel.addEventListener("click", e=>{
+      e.preventDefault();
+      this.setCookie("today", "done", 0);
+      });
+   
+      //팝업 닫기 버튼 이벤트
+      this.btnClose.addEventListener("click", e=>{
+         this.popup.style.display = "none";
+   
+         let isChecked = this.popup.querySelector("input[type=checkbox]").checked;
+         if(isChecked) this.setCookie("today", "done", 1);
+      });
+   }
 
-const isCookie = document.cookie.indexOf("today=done");
-let isOn;
 
-
-
-/*
-(isCookie == -1) ? isOn = "block" : isOn ="none"; 아래와 같은 코드
-*/
-if(isCookie == -1){ // -1 => 쿠키가 없다면
-   console.log("쿠키없음");
-   isOn = "block";
-}else{
-   console.log("쿠키있음");
-   isOn = "none";
+setCookie(name, val, due){
+      const today = new Date();
+      const date = today.getDate();
+      today.setDate(date + due);
+      const duedate = today.toGMTString();
+      document.cookie = `${name}=${val}; path=/; expires = ${duedate}`;
+   }
 }
-popup.style.display = isOn;
 
 
 
-//쿠키확인 이벤트
-btnView.addEventListener("click", e=>{
-   e.preventDefault();
-   console.log(document.cookie);
-
-});
-
-//쿠키 확인 버튼 이벤트
-btnDel.addEventListener("click", e=>{
-   e.preventDefault();
-   setCookie("today", "done", 0);
-});
-
-//팝업 닫기 버튼 이벤트
-btnClose.addEventListener("click", e=>{
-   popup.style.display = "none";
-
-   let isChecked = popup.querySelector("input[type=checkbox]").checked;
-   if(isChecked) setCookie("today", "done", 1);
-});
-
-
-function setCookie(name, val, due){
-   const today = new Date();
-   const date = today.getDate();
-   today.setDate(date + due);
-   const duedate = today.toGMTString();
-   document.cookie = `${name}=${val}; path=/; expires = ${duedate}`;
-}
